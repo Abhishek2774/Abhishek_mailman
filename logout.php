47ca30f4422ee72efa8a831f1d413ae072e27884
Abhishek Kumar <75356361+errorkeda@users.noreply.github.com>
2022-07-19 17:02:12 +0530

    check after edit logout

--- logout.php
+++ logout.php
@@ -2,8 +2,7 @@
 session_start(); //to ensure you are using same session
 session_unset();
 session_destroy(); //destroy the session
-// header("location:http://hestalabs.com/tse/Abhishek_mailman/index.php"); //to redirect back to "index.php" after logging out
-header("location:http://localhost/Abhishek_mailman/index.php"); //to redirect back to "index.php" after logging out
-
+header("location:http://hestalabs.com/tse/Abhishek_mailman/index.php"); //server
+// header("location:http://localhost/Abhishek_mailman/index.php"); // local
 exit();
 ?>
