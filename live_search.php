ce9dba8d6a058d5b4ab420955d2bb45a7a8083c8
Abhishek Kumar <75356361+errorkeda@users.noreply.github.com>
2022-07-19 10:38:13 +0530

    update serch live

--- live_search.php
+++ live_search.php
@@ -1,11 +1,12 @@
 <?php  
+include "autoload.php";
+$gobj = new Database();
  $search_value = $_POST["search"];
-$conn = new mysqli("localhost","root","hestabit","bitmail") or die("connection failed");
-$sql = "SELECT * FROM All_emails WHERE  reciver_email like '%{$search_value}%' ";//OR age Like '%{$search_value}%'
-  $result = mysqli_query($conn, $sql) or die("sql query failed");
+$sql = "SELECT * FROM All_emails WHERE  subject like '%{$search_value}%' ";
+  $result = $gobj->mysqli->query($sql) or die("sql query failed");
 $output = "";
 
-if(mysqli_num_rows($result) > 0){
+if($gobj->mysqli->num_rows > 0){
     $output .= '<table border="1" width="100px" cellspacing="0" cellpadding="10px">';
             while($row = mysqli_fetch_assoc($result)){
                 
